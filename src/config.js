ddbd6854609415f15cc95478f1ba9ccd4bc451b2
Santiago Alexis Quiroz Bazan <saquirozbazan@gmail.com>
2017-02-25 16:17:07 -0500

    updated patterns: yes & no

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports =  {
   ICON_EMOJI_TO_REACT: 'heart',
   USE_CASES: 'direct_message,direct_mention,mention',
   PATTERN: {
-    yes: 'yes|yea|yup|yep|ya|sure|ok|y|yeah|yah|aye|dale|yep|okay|ok',
-    no: 'no|nah|nope|n|nop|nai|nel|fak u'
+    yes: '(yes|yea|yup|yep|ya|sure|ok|y|yeah|yah|aye|dale|yep|okay|ok)(.*)',
+    no: '(no|nah|nope|n|nop|nai|nel|fak u)(.*)'
   }
 }
